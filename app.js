d6bc5ac29cce59c59d11bc07e63099477ec9f6af
Eunji Song <eunji.l.song@gmail.com>
2018-01-16 14:45:52 -0500

    work in the afternoon

--- app.js
+++ app.js
@@ -1,44 +1,69 @@
 const express = require('express');
 const app = express();
+const nunjucks = require('nunjucks');
+const tweetBank = require('./tweetBank')
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+nunjucks.configure('views', {noCache: true});
+
+app.set('view engine', 'html'); 
+app.engine('html', nunjucks.render);
 
 
-// let's ask which one is the best way to use USE : seperate or one 
+//////////////////// MIDDLEWARE /////////
 app.use(function(req, res, next){ 
-    
-    // if(req.url === '/special'){
-    //     console.log('You reached the special area')
-    // }
 
-    //console.log(res.statusCode(200).json());
     console.log(req.method, res.statusCode)
     next();
 })
 
 app.use('/special/', function(req, res, next){
-    // how do we access to /special/ 
+    
     console.log('You reached the special area')
     next();
 })
 
+app.use('/tweet/5', function(req, res, next){
+    
+    console.log(tweetBank.getFakeTweet())
+    next();
+})
 
 
-//////////////////////////////
+////////////////////////////// METHOD //// 
 
 app.get('/', function(req, res){
     res.send('Hello World')
-    //console.log(req.method, req.url);
 
 } )
+
 app.get('/news', function(req, res){
     res.send('hihi')
-    //console.log(res.status());
 })
 
 app.get('/special/', function(req, res){
     res.send('You\' reached the special area')
 })
 
-///////////////////////////////////
+app.get('/views', function(req, res){
+    res.render('index', locals);
+    
+})
+
+// let's ask the better way to implement this. it's more usable 
+app.get('/tweet/5', function(req, res){
+    res.send(tweetBank.list()[4].content)
+})
+
+/////////////////////////////////// LISTENING  /////
 
 app.listen(3000, function(){
     console.log('server listening')
